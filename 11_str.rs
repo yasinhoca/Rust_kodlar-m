fb81020f3b583531fd07e334527199b1f0855aae
Yasin Hoca <myasinozsaglam@gmail.com>
2022-07-05 00:08:55 +0300

    Create 11_str.rs

---
+++ 11_str.rs
@@ -0,0 +1,17 @@
+let s = "Hello".to_string();
+
+let s = String::from("world");
+let s: String = "also this".into();
+
+
+let s = "Hello".to_string(); //toplam
+let message = s + " world!";
+
+
+et sparkle_heart = vec![240, 159, 146, 150];
+
+assert_eq!("💖", sparkle_heart);
+
+let bytes = sparkle_heart.into_bytes();
+
+assert_eq!(bytes, [240, 159, 146, 150]);
